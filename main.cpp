dbedc281bff23339f88cb634f62571f5ac1c48a9
ELJTA <95251914+ELJTA@users.noreply.github.com>
2021-12-13 14:45:01 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,208 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main() {
+char choice;
+cout << "Pick a confrence" << endl;
+cout << "A - NFC" << endl;
+cout << "B - AFC" << endl;
+cin >> choice;
+if (choice == 'A') { 
+  cout << "Then select a team that shows the starting running backs stats" <<endl;}
+  
+  if (choice == 'A') {
+  cout << "A - Green Bay Packers" << endl;
+  cout << "B - Los Angeles Rams"<< endl;
+  cout << "C - Tampa Bay Buccaneers"<< endl;
+  cout << "D - San Fransico 49ers"<< endl;
+  cout << "E - Arizona Cardnials"<< endl;
+  cout << "F - Philidalphia Eagles"<< endl;
+  cout << "G - Minnesota Vikings"<< endl;
+  cout << "H - Seattle Seahawks"<< endl;
+  cout << "I - New Orleans Saints"<< endl;
+  cout << "J - Carolina Panthers"<< endl;
+  cout << "K - Chicago Bears"<< endl;
+  cout << "L - New York Giants"<< endl;
+  cout << "M - Washington Football Team"<< endl;
+  cout << "N - Detroit Lions"<< endl;
+  cout << "O - Atlanta Falcons"<< endl;
+  cout << "P - Dallas Cowboys" << endl;
+  cin >> choice;
+if (choice == 'A') {
+  cout << "Aaron Rodgers\n" "Cmp% - 66.2% \n" "yds - 2878\n" "TD - 23 \n";}
+
+if (choice == 'B') {
+  cout << "Mathew Stafford\n" "Cmp% - 66.5% \n" "yds - 3316\n" "TD - 27 \n";}
+
+if (choice == 'C') {
+  cout << "Tom Brady\n" "Cmp% - 67.6% \n" "yds - 3403\n" "TD - 30 \n";}
+
+if (choice == 'D') {
+  cout << "Jimmy Garoppolo\n" "Cmp% - 66.8% \n" "yds - 2342\n" "TD - 13 \n";}
+
+if (choice == 'E') {
+  cout << "Kyle Murray\n" "Cmp% - 72.7% \n" "yds - 2276\n" "TD - 17 \n";}
+  
+if (choice == 'F') {
+  cout << "Jalen Hurts\n" "Cmp% - 60.1% \n" "yds - 2435\n" "TD - 13 \n";}
+
+if (choice == 'G') {
+  cout << "Kirk Cousins\n" "Cmp% - 67.7% \n" "yds - 3013\n" "TD - 23 \n";}
+
+if (choice == 'H') {
+  cout << "Russel Wilson\n" "Cmp% - 64.9% \n" "yds - 1811\n" "TD - 12 \n";}
+
+if (choice == 'I') {
+  cout << "Russel Wilson\n" "Cmp% - 59.0% \n" "yds - 1170\n" "TD - 14 \n";}
+
+if (choice == 'J') {
+  cout << "Cam Newton\n" "Cmp% - 55.8% \n" "yds - 289\n" " TD - 3 \n";}
+
+if (choice == 'K') {
+  cout << "Justin Fields\n" "Cmp% - 58.1% \n" "yds - 1361\n" "TD - 4 \n";}
+
+  if (choice == 'L') {
+  cout << "Daniel Jones\n" "Cmp% - 64.3% \n" "yds - 2428\n" "TD - 12 \n";}
+
+  if (choice == 'M') {
+  cout << "Jared Goff\n" "Cmp% - 67.3% \n" "yds - 2280\n" "TD - 10 \n";}
+
+if (choice == 'P') {
+  cout << "Taylor Heinicke\n" "Cmp% - 67.2% \n" "yds - 2613\n" "TD - 16 \n";}
+
+if (choice == 'N') {
+  cout << "Matt Ryan\n" "Cmp% - 67.5% \n" "yds - 2617\n" "TD - 16 \n";}
+
+if (choice == 'O') {
+  cout << "Dak Prescott\n" "Cmp% - 69.4% \n" "yds - 2932\n" "TD - 22 \n";}
+
+
+
+  }
+  if (choice =='B') {
+
+    cout << "Ok then select a team that shows the starting running backs stats\n";
+} 
+if (choice == 'B') {
+ cout << "a - New England" << endl;
+ cout << "b - Kansas City Chiefs" << endl;
+ cout << "c - Buffalo Bills"<< endl;
+ cout << "d - Las vegas Raiders"<< endl;
+ cout << "e - Tennese Titans"<< endl;
+ cout << "f - Los Angeles Chargers"<< endl;
+ cout << "g - Indianapolis Colts"<< endl;
+ cout << "h - Cleveland Browns"<< endl;
+ cout << "i - Cincinnati Bengals"<< endl;
+ cout << "j - Denver Broncos"<< endl;
+ cout << "k - Miami Dolphins"<< endl;
+ cout << "l - New York Jets"<< endl;
+ cout << "m - Jacksonville Jaguars"<< endl;
+ cout << "n - Houston Texans"<< endl;
+ cout << "o - Pittsburg Stealers"<< endl;
+ cout << "p - Balimore Ravens"<< endl;
+cin >> choice;}
+if (choice == 'a') {
+cout << "Mac Jones\n" "CMP% - 70.3 \n" "yds -2850\n" "TD - 16\n";
+} 
+if (choice == 'b') {
+  cout << "Patrick Mahomes\n" "CMP - 65.5\n" "yds - 3200\n" "tds - 139\n ";
+}
+if (choice == 'c') {
+cout << "Josh Allen\n" "Cmp - 66.8\n" "yds - 3071\n" "TD - 25";
+}
+if (choice == 'd'){
+cout << "Derek Carr\n Cmp - 67.3\n Yds - 3414\n Td - 17";
+}
+if (choice == 'e'){
+cout << "Ryan Tannehill\n" "Cmp - 65.8" "Yds - 2774" "Td - 14";
+}
+if (choice == 'f'){
+cout << "Justin Herbert\n" "Cmp - 66\n Yds - 3230\n Td - 24\n";
+}
+if (choice == 'g'){
+cout << "Carson Wentz\n" "Cmp - 62.8\n Yds - 2790\n Td - 21\n";
+}
+if (choice == 'h'){
+cout << "Baker Mayfield\n" "Cmp - 62.2\n Yds - 2413\n Td - 11\n";
+}
+if (choice == 'i'){
+cout << "Joe Burrow\n" "Cmp - 69.3\n Yds - 2835\n Td - 22\n";
+}
+if (choice == 'j'){
+cout << "Teddy Bridgwater\n" "Cmp\n - 68.7 Yds - 2518\n Td - 15\n";
+}
+if (choice == 'k'){
+cout << "Tua Tagovailoa\n" "Cmp\n - 70.5 Yds - 1701\n Td - 10\n";
+}
+if (choice == 'l'){
+cout << "Zach Wilson\n" "Cmp\n - 57.6 Yds - 1313\n Td - 4\n";
+}
+if (choice == 'm'){
+cout << "Trevor Larwence\n" "Cmp\n - 58 Yds - 2369\n Td - 9\n";
+}
+if (choice == 'n'){
+cout << "David Mills\n" "Cmp\n - 67 Yds - 1357\n Td - 7\n";
+}
+if (choice == 'o'){
+cout << "Ben Roethlisberger\n" "Cmp\n - 64.6 Yds - 2522\n Td - 14\n";
+}
+if (choice == 'p'){
+cout << "Lamar Jackson\n" "Cmp\n - 64.2 Yds - 2612\n Td - 15\n";
+}
+
+{
+cout << "\nDo you want to see them ranked?\n";
+cout << "A - Yes" << endl;
+cout << "B - No" << endl;
+cin >> choice;
+if (choice == 'A')
+
+cout << " 1st Aaron Rodgers\n ";
+cout << " 2nd Tom Brady\n "; 
+cout << " 3rd Kyler Murray\n ";
+cout << " 4th Matthew Stafford \n";
+cout << " 5th Dak Prescott\n ";
+cout << " 6th Josh Allen\n ";
+cout << " 7th Patrick Mahomes\n ";
+cout << " 8th Justin Herbert\n ";
+cout << " 9th Lamar Jackson\n ";
+cout << " 10th Kirk Cousins\n";
+cout << " 11th Derek Carr \n";
+cout << " 12th Russell Wilson\n ";
+cout << " 13th Joe Burrow\n ";
+cout << " 14th Carson Wentz\n ";
+cout << " 15th Jimmy Garoppolo \n";
+cout << " 16th Ryan Tannehill\n ";
+cout << " 17th Jalen Hurts\n";
+cout << " 18th Matt Ryan\n";
+cout << " 19th Ben Roethlistberger\n";
+cout << " 20th Taylor Heinicke\n";
+cout << " 21st Mac Jones\n";
+cout << " 22nd Tua Tagovailoa\n";
+cout << " 23rd Teddy Bridgwater\n";
+cout << " 24th Danile Jones\n";
+cout << " 25th Baker Mayfield\n";
+cout << " 26th Cam Newton\n";
+cout << " 27th Jared Goff\n"; 
+cout << " 28th Tyrod Taylor\n";
+cout << " 29th Andy Dalton\n";
+cout << " 30th Jamies Winston\n";
+cout << " 31st Trevor Larwence\n";
+cout << " 32nd Zach Wilson\n";
+
+if (choice == 'B')
+cout << "Ok then hope you liked our stats list :)";
+
+
+
+
+}
+
+
+
+
+
+
+}
